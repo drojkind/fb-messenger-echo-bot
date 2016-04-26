7ba7ff57fd75757fefe3bf23f648b2b6acb31fe4
Daniel Rojkind <daniel@dpc.io>
2016-04-26 16:51:02 -0500

    Changes

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 var conf = module.exports = {
 	FB_MESSAGE_URL: 'https://graph.facebook.com/v2.6/me/messages',
-	VERIFY_TOKEN: 'verify token here',
+	VERIFY_TOKEN: 'this_is_a_verify_token_drojkind',
 	PROFILE_TOKEN: 'page access token here',
 	PORT: '3000'
 };
