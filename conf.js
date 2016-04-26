0c4692c56a975b0f0dc8ade5e863cdff89af5646
Daniel Rojkind <daniel@dpc.io>
2016-04-26 18:22:03 -0500

    yes yes yes

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 var conf = module.exports = {
 	FB_MESSAGE_URL: 'https://graph.facebook.com/v2.6/me/messages',
 	VERIFY_TOKEN: 'this_is_a_verify_token_drojkind',
-	PROFILE_TOKEN: 'page access token here',
+	PROFILE_TOKEN: 'CAAY2KxYShPUBAAH4l8vHmRqZBq22FkkGVxTGQYNOMcE9bwdQNgGm5eVNBmfW8t9O4idfycRTLEfbyVm4iXGqSCb4e9jYUHZBzjLjeRpZAZAZBdmYVIURJZAglqaYZCXlmCZBFUXfnyoJSVtYQGZC4aTTVSDN1dSJcoeS7jKSScBSgKPCxEtYmovmeze209hcGT53b6XdJl2ZCEEwZDZD',
 	PORT: '3000'
 };
