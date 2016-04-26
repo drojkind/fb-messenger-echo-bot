caf0d9ce6432525e41d3a6abe448bb67475bb930
root <root@SinEspera.io>
2016-04-26 18:37:20 -0400

    yoplo

--- test.js
+++
@@ -1,9 +0,0 @@
-var https = require('https');
-var fs = require('fs');
-var options = {
-     key: fs.readFileSync('/etc/letsencrypt/live/sinespera.io/privkey.pem'),
-     cert: fs.readFileSync('/etc/letsencrypt/live/sinespera.io/fullchain.pem'),
-     ca: fs.readFileSync('/etc/letsencrypt/live/sinespera.io/chain.pem')
-}
-var server = https.createServer(options, handlerFunction);
-server.listen(8080, '127.0.0.1');
